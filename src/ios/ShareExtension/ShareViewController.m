343bf4cbdcaf12405f4d98b151c11fd316959fac
momoterraw <andrewvmail@gmail.com>
2020-09-22 14:45:48 -0700

    Handle public.file-url

--- src/ios/ShareExtension/ShareViewController.m
+++ src/ios/ShareExtension/ShareViewController.m
@@ -448,7 +448,56 @@
                     [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil]; }];
                 return;
             }
-     
+            
+            if ([itemProvider hasItemConformingToTypeIdentifier:@"public.file-url"]) {
+                  [itemProvider loadItemForTypeIdentifier:@"public.file-url" options:nil completionHandler:^(NSURL *url, NSError *error) {
+                      NSLog(@"dataPath------------------------------------------------------------");
+                      [self debug:url.absoluteString];
+                      NSString *uti = @"";
+
+                      NSError* readError = nil;
+                      NSData *data = [NSData dataWithContentsOfURL:url options: 0 error: &readError];
+                      if (data == nil) {
+                          NSLog(@"Failed to read file, error %@", readError);
+                      }
+
+                      NSUUID *uuid = [NSUUID UUID];
+                      NSString *str = [uuid UUIDString];
+
+                      NSURL  *containerURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:SHAREEXT_GROUP_IDENTIFIER];
+
+                      NSString *dot = @".";
+                      NSString *ext = [dot stringByAppendingString: [url.absoluteString pathExtension]];
+
+                      NSString *dataPath =
+                      [containerURL.absoluteString stringByAppendingPathComponent: [str stringByAppendingString:ext]];
+                      containerURL = [containerURL URLByAppendingPathComponent:[NSString stringWithFormat: @"%@", [str.lowercaseString stringByAppendingString:ext] ]];
+
+                      NSError* writeError = nil;
+                      [[NSFileManager defaultManager] createFileAtPath:dataPath contents:nil attributes:nil];
+                      [data writeToURL:containerURL options: NSDataWritingAtomic error: &writeError];
+
+                      NSArray<NSString *> *utis = [NSArray new];
+                      if ([itemProvider.registeredTypeIdentifiers count] > 0) {
+                          uti = itemProvider.registeredTypeIdentifiers[0];
+                          utis = itemProvider.registeredTypeIdentifiers;
+                      }
+                      NSDictionary *dict = @{
+                          @"text": self.contentText,
+                          @"backURL": self.backURL,
+                          @"data" : [[NSData alloc] init],
+                          @"uti": uti,
+                          @"utis": utis,
+                          @"name": containerURL.absoluteString
+                      };
+                      [self.userDefaults setObject:dict forKey:@"image"];
+                      [self.userDefaults synchronize];
+                      NSString *urlApp = [NSString stringWithFormat:@"%@://image", SHAREEXT_URL_SCHEME];
+                      [self openURL:[NSURL URLWithString:urlApp]];
+                      [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil]; }];
+                  return;
+              }
+
              // URL
              if ([itemProvider hasItemConformingToTypeIdentifier:@"public.url"]) {
                 [itemProvider loadItemForTypeIdentifier:@"public.url" options:nil completionHandler:^(NSURL *url, NSError *error) {
